054158f517753454616a19977f88eceb404ad771
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-10-16 11:21:25 -0300

    created embed

---
+++ components/embed.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Callout } from "nextra-theme-docs";
+
+export function Embed({ src }) {
+  return (
+    <iframe
+      width="100%"
+      height="450"
+      src={src}
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    />
+  );
+}
+
+export function IREmbed({ src }) {
+  return (
+    <div>
+      <Embed src={src} />
+      <Callout type="info">
+        El video completo disponible en{" "}
+        <a
+          href="https://ready.silver.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Interview Ready
+        </a>
+        .
+      </Callout>
+    </div>
+  );
+}
